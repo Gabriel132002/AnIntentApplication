4273b3caea9b86d6a3165c64d51a8bfc19b4ef69
Gabrie Rodrigues <gabriel02aguiar@gmail.com>
2024-03-05 18:57:03 -0300

    intentApplication

---
+++ app/src/main/java/com/example/myintentapplication/SecondScreen.java
@@ -0,0 +1,33 @@
+package com.example.myintentapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondScreen extends AppCompatActivity {
+
+    Button btnVoltarParaPrimeiraTela;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_screen);
+
+        btnVoltarParaPrimeiraTela = findViewById(R.id.btn_back_to_main_screen);
+
+        btnVoltarParaPrimeiraTela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SecondScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+    }
+}
